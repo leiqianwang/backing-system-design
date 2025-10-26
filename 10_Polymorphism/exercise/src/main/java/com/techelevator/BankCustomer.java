569e92fa90cb5764473edb5cf77458376eb9b362
Ying <42222462+leiqianwang@users.noreply.github.com>
2025-10-26 17:11:41 -0400

    Add files via upload

---
+++ 10_Polymorphism/exercise/src/main/java/com/techelevator/BankCustomer.java
@@ -0,0 +1,101 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BankCustomer {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private List<Accountable> accounts = new ArrayList<>();
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    //public List<Accountable> getAccounts(){
+    //       return accounts;
+
+    public Accountable[] getAccounts() {
+        Accountable[] account = new Accountable[accounts.size()];
+
+        for (int i = 0; i < accounts.size(); i++) {
+            account[i] = accounts.get(i);
+
+        }
+        return account;
+    }
+
+
+    public void addAccount(Accountable newAccount) {
+
+        accounts.add(newAccount);
+    }
+
+    public boolean isVip() {
+        boolean isVip = false;
+        int sum = 0;
+        int minimum = 25000;
+
+        for (Accountable account : accounts) {
+            sum += account.getBalance();
+            if (sum < minimum) {
+                isVip = false;
+            }
+            if (sum >= 25000) {
+                isVip = true;
+            }
+        }
+
+        return isVip;
+    }
+
+
+    public static void main(String[] args) {
+        BankCustomer customer = new BankCustomer();
+        customer.setName("John Smith");
+        customer.setAddress("123 Main St");
+        customer.setPhoneNumber("555-1234");
+
+        // Add some accounts to the customer
+        SavingsAccount savingsAccount = new SavingsAccount("Savings123", "5000");
+        CheckingAccount checkingAccount = new CheckingAccount("Checking456", "10000", 1000);
+        CreditCardAccount creditCardAccount = new CreditCardAccount("Credit789", "123456789");
+        creditCardAccount.charge(5000);
+        customer.addAccount(savingsAccount);
+        customer.addAccount(checkingAccount);
+        customer.addAccount(creditCardAccount);
+
+        // Check if the customer is a VIP
+        if (customer.isVip()) {
+            System.out.println(customer.getName() + " is a VIP.");
+        } else {
+            System.out.println(customer.getName() + " is not a VIP.");
+        }
+    }
+}
+
